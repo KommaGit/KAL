c4ed4cda7b8b3f3bbcac3f669b865f8296e17715
Pascal <5804070+PaLemmen@users.noreply.github.com>
2020-04-17 00:04:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+#Komma Animation Library
+
+Welcome at the Komma Animation Library or for short **KAL**.
+
+Will add the *little sugar* that your new site needs.
+By defining the right data properties and some additional custom css properties, it brings some additional awesomeness.
+
+This package has the trigger mechanism for toggling the classes for animation.
+Also some default animation have been defined that can be use outside of the box.
+
+If you're really are in a lot of leftover time you could also write your own animation and use that.
+As long as you use the same syntax/states selector for the animation. 
+
+## Demo
+
+Are your curious to what our animation library can do?
+Take a look around our own basic website; http://kommabasic.nl/
+
+## Installation
+
+```bash
+# With npm
+npm i @komma/kal
+```
+
+## Usage
+
+Import the library into your app.js.
+And just run the init function.
+
+```script
+import {KAL} from '@komma/kal'
+KAL.init();
+```
+
+Then al you need to do is defined the elements you want to animate.
+
+
+### Examples
+
+Here are some examples:
+
+```html
+<h1 class="c-my-title"  data-kal="zoom-in">
+   Just an example
+</h1>
+
+<h2 class="c-my-title"  data-kal="slide-up" style="--kal-duration: 2s; ">
+   Another example
+</h2>
+
+<h3 class="c-my-title"  data-kal="slide-up" data-kal-once="false">
+   And another one
+</h3>
+```
+
+## License
+
+MIT
---
+++ kal.js
@@ -0,0 +1,154 @@
+const KAL = {
+
+    /**
+     * The data attribute prefix we use for our animation library
+     * @type string
+     */
+    dataPrefix: 'kal',
+
+    /**
+     * Define the class we used to set the states of the animations
+     */
+    startStateClass: 'is-kal-start',
+    endStateClass: 'is-kal-end',
+
+    /**
+     * Empty basket for putting the NodeElements in
+     */
+    elements: [],
+
+    /**
+     * Default options
+     */
+    options: {
+        root: null,
+        rootMargin: '-10% 0% -10% 0%',
+        threshold: 0.5, // The visual percentage of component when animation is triggered
+    },
+
+    /**
+     * Element default options
+     */
+    elementOptions: {
+        once: true,
+    },
+
+    /** @type {IntersectionObserver} */
+    intersectionObserver: null,
+
+    init() {
+
+        this.elements = document.querySelectorAll('*[data-'+ KAL.dataPrefix +']');
+        if(this.elements.length === 0 ) return;
+
+        if (!window.IntersectionObserver) {
+            console.warn('IntersectionObserver is not supported');
+            return;
+        }
+
+        KAL.createIntersectionObserver();
+    },
+
+    /**
+     * Create the Intersection Observer
+     * and append the KAL elements into it.
+     */
+    createIntersectionObserver() {
+
+        KAL.intersectionObserver = new IntersectionObserver(KAL.onIntersection, {
+            root: KAL.options.root,
+            rootMargin: KAL.options.rootMargin,
+            threshold: KAL.options.threshold,
+        });
+
+        console.log(KAL.intersectionObserver);
+
+        KAL.elements.forEach((element) => {
+            KAL.intersectionObserver.observe(element);
+            element.classList.add(KAL.startStateClass);
+        });
+    },
+
+    /**
+     * IntersectionObserver callback
+     * @param  {Array<IntersectionObserverEntry>} entries
+     * @param  {IntersectionObserver} observer
+     */
+    onIntersection(entries, observer) {
+        entries.forEach((entry) => {
+
+            console.log(entry.intersectionRatio);
+
+            // When element's is in viewport and within threshold,
+            // Trigger animation!
+            if (entry.intersectionRatio >= KAL.options.threshold) {
+
+                KAL.triggerAnimation(entry);
+
+                if (KAL.getElementOption(entry.target, 'once')) {
+                    observer.unobserve(entry.target);
+                }
+            } else if (!KAL.getElementOption(entry.target, 'once')) {
+
+                // Reverse the animation if options is not once
+                KAL.reverseAnimation(entry);
+            }
+        });
+    },
+
+    /**
+     * Clear the intersection observer.
+     */
+    clearIntersectionObserver() {
+      KAL.intersectionObserver.disconnect();
+      KAL.intersectionObserver = null;
+    },
+
+    /**
+     * Get an animation option of the element.
+     *
+     * @param element
+     * @param attribute
+     */
+    getElementOption(element, attribute)
+    {
+        const option = element.getAttribute('data-'+ KAL.dataPrefix + '-' + attribute);
+
+        if(option !== null){
+
+            // Map the attributes values into the right type
+            switch (attribute) {
+
+                // Boolean attributes
+                case 'once':
+                    return (option == 'true');
+
+                default:
+                    return option;
+            }
+        }
+        return KAL.elementOptions[attribute];
+    },
+
+    /**
+     * Trigger animation
+     * @param {IntersectionObserverEntry} entry
+     */
+    triggerAnimation(entry) {
+        const element = entry.target;
+        element.classList.remove(KAL.startStateClass);
+        element.classList.add(KAL.endStateClass);
+    },
+
+    /**
+     * Reverse animation
+     * @param {IntersectionObserverEntry} entry
+     */
+    reverseAnimation(entry) {
+        const element = entry.target;
+        element.classList.add(KAL.startStateClass);
+        element.classList.remove(KAL.endStateClass);
+    },
+}
+
+export { KAL }
